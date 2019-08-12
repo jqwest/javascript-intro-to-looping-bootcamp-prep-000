02d196d46fdae82e3b0fe14441c20e9189966c82
john L <johnleavell@gmail.com>
2019-08-12 02:37:44 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,3 +23,12 @@ function whileLoop(n){
 function maybeTrue() {
   return Math.random() >= 0.5
 }
+function doWhileLoop(array){
+do {
+  array.pop()
+} while (array.length>0 && maybeTrue())
+return array
+}
+
+do_while_arr = ['a','b','c'];
+console.log(doWhileLoop(do_while_arr))
